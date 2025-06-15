081666ff73dd85d69f0a3e6d6e5b6086c7804e68
Anthony Ponte <anthonyrponte@gmail.com>
2025-06-15 17:18:42 -0500

    Primera version estable

--- src/main/java/com/anthonyponte/eventos/controller/EventoController.java
+++ src/main/java/com/anthonyponte/eventos/controller/EventoController.java
@@ -1,19 +1,19 @@
 package com.anthonyponte.eventos.controller;
 
-import java.util.Map;
+import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.anthonyponte.eventos.entity.Evento;
 import com.anthonyponte.eventos.service.EventoService;
@@ -24,64 +24,34 @@ public class EventoController {
 	@Autowired
 	private EventoService service;
 
-	@Autowired
-	private Environment environment;
-
 	@GetMapping
-	public Map<String, Object> listarEventos() {
-		return Map.of(
-				"POD_NAME", environment.getProperty("POD_NAME", "Unknown"),
-				"POD_ID", environment.getProperty("POD_ID", "Unkown"),
-				"eventos", service.listarEventos());
+	public List<Evento> listarEventos() {
+		return service.listarEventos();
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Evento> obtenerEventoPorId(@PathVariable Long id) {
-		return service.obtenerEventoPorId(id).map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+		Evento e = service.obtenerEventoPorId(id);
+		return ResponseEntity.ok(e);
 	}
 
 	@PostMapping
-	public ResponseEntity<Evento> guardarEvento(@RequestBody Evento evento) {
-		return ResponseEntity.ok(service.guardarEvento(evento));
+	public ResponseEntity<?> guardarEvento(@RequestBody Evento evento) {
+		Evento e = service.guardarEvento(evento);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.build(e.getId());
+		return ResponseEntity.created(uri).build();
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<Evento> actualizarEvento(@PathVariable Long id, @RequestBody Evento evento) {
-		return service.actualizarEvento(id, evento)
-				.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+		Evento e = service.actualizarEvento(id, evento);
+		return ResponseEntity.ok(e);
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> eliminarEvento(@PathVariable Long id) {
-		service.eliminarEvento(id);
-		return ResponseEntity.noContent().build();
-	}
-
-	@GetMapping("/{id}/existe")
-	public ResponseEntity<?> existeEventoPorId(@PathVariable Long id) {
-		boolean existe = service.existeEventoPorId(id);
-		return existe ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
-	}
-
-	@PatchMapping("/{id}/disminuir-capacidad-max")
-	public ResponseEntity<?> disminuirCapacidadMax(@PathVariable Long id) {
-		return service.obtenerEventoPorId(id).map(evento -> {
-			if (evento.getCapacidadMax() == 0) {
-				return ResponseEntity.badRequest()
-						.body(Map.of("mensaje", "El evento no tiene capacidad disponible"));
-			}
-			evento.setCapacidadMax(evento.getCapacidadMax() - 1);
-			return ResponseEntity.ok(service.guardarEvento(evento));
-		}).orElse(ResponseEntity.notFound().build());
-	}
-
-	@PatchMapping("/{id}/aumentar-capacidad-max")
-	public ResponseEntity<?> aumentarCapacidadMax(@PathVariable Long id) {
-		return service.obtenerEventoPorId(id).map(evento -> {
-			evento.setCapacidadMax(evento.getCapacidadMax() + 1);
-			return ResponseEntity.ok(service.guardarEvento(evento));
-		}).orElse(ResponseEntity.notFound().build());
+	public void eliminarEventoPorId(@PathVariable Long id) {
+		service.eliminarEventoPorId(id);
 	}
 }
--- src/main/java/com/anthonyponte/eventos/service/EventoService.java
+++ src/main/java/com/anthonyponte/eventos/service/EventoService.java
@@ -1,7 +1,6 @@
 package com.anthonyponte.eventos.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -11,13 +10,11 @@ import com.anthonyponte.eventos.entity.Evento;
 public interface EventoService {
 	public List<Evento> listarEventos();
 
-	public Optional<Evento> obtenerEventoPorId(Long id);
+	public Evento obtenerEventoPorId(Long id);
 
 	public Evento guardarEvento(Evento evento);
 
-	public Optional<Evento> actualizarEvento(Long id, Evento evento);
+	public Evento actualizarEvento(Long id, Evento evento);
 
-	public void eliminarEvento(Long id);
-
-	public boolean existeEventoPorId(Long id);
+	public void eliminarEventoPorId(Long id);
 }
